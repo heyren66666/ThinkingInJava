d42b867700ac5644ce270bc490716ebd30f98756
heyren66666 <1873714818@qq.com>
2020-10-26 14:24:56 +0800

    Practice11

---
+++ src/Practice11.java
@@ -0,0 +1,11 @@
+public class Practice11 {
+
+    static int a;
+    static char b;
+    public static void main(String[] args) {
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+
+
+}
